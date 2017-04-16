c22031370ff7d9bcb63f9366ff193be1d22a3c5b
smithwebtek <brad@smithwebtek.com>
2017-04-16 13:07:36 -0400

    reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  root 'static#home'
-  get '/about', to: 'static#about'
-  get '/blog', to: 'static#blog'
-  get '/projects', to: 'static#projects'
-  get '/wiki', to: 'static#wiki'
-  get '/tools', to: 'static#tools'
-  get '/contact', to: 'static#contact'
   resources :posts
-# resources :tags, :users, :comments, :sections, :posts
-
+   root 'static#home'
 end
