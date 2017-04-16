bd08669b8f68b62c4d1a723664f09d71ae2fe67e
smithwebtek <brad@smithwebtek.com>
2017-04-16 10:38:36 -0400

    draft about page, partialize footer and common page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,4 +3,28 @@ class StaticController < ApplicationController
   def home
     render :home
   end
+
+  def about
+    render 'static/about'
+  end
+
+  def blog
+    render 'static/blog'
+  end
+
+  def projects
+    render 'static/projects'
+  end
+
+  def wiki
+    render 'static/wiki'
+  end
+
+  def tools
+    render 'static/tools'
+  end
+
+  def contact
+    render 'static/contact'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root 'static#home'
+  get '/about', to: 'static#about'
+  get '/blog', to: 'static#blog'
+  get '/projects', to: 'static#projects'
+  get '/wiki', to: 'static#wiki'
+  get '/tools', to: 'static#tools'
+  get '/contact', to: 'static#contact'
   resources :posts
-   root 'static#home'
+# resources :tags, :users, :comments, :sections, :posts
+
 end
