bb31246851b73b950066df087eb23ba2047c6ba1
smithwebtek <brad@smithwebtek.com>
2017-04-16 13:43:49 -0400

    recovery 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :posts
   root 'static#home'
+
+  get '/about', to: 'static#about'
+  get '/blog', to: 'posts#index'
+  get '/projects', to: 'static#projects'
+  get '/wiki', to: 'static#wiki'
+  get '/tools', to: 'static#tools'
+  get '/contact', to: 'static#contact'
   
+  resources :posts
 end
