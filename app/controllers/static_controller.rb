ec95e426cc90bdd7c06b0ef1b5c775bfdc3c9e56
smithwebtek <brad@smithwebtek.com>
2017-04-18 08:17:28 -0400

    fussing with styling

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,18 +3,23 @@ class StaticController < ApplicationController
   def home
     render :home
   end
+
+  def about
+    render :about
+  end
+
   def contact
     render :contact
   end
+  
   def projects
     render :projects
   end
-  def tools
-    render :tools
-  end
+  
   def wiki
     render :wiki
   end
+  
   def blog
     redirect_to posts_path
   end
