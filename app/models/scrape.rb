d8dc490c9754c2c6b1060bceed3e75a333263c10
smithwebtek <brad@smithwebtek.com>
2018-03-01 07:23:10 -0500

    Setting up library on github

--- app/models/scrape.rb
+++ app/models/scrape.rb
@@ -1,5 +1,7 @@
 class Scrape < ApplicationRecord
 require 'open-uri'
+require 'pdf/reader'
+  
 
 	# every few weeks, export and replace the bookmarks.html file, (exported from Chrome)
 	# until you find a way to reach out to Chrome programatically
@@ -25,6 +27,7 @@ require 'open-uri'
 		books = []
 		Dir.chdir(Rails.root.join('app', 'assets', 'books'))
 		folders = Dir.glob('*')
+		puts folders
 		folders.each do |folder|
 			Dir.chdir(Rails.root.join('app', 'assets', 'books' + '/' + "#{folder}"))
 			files = Dir.glob('*')
@@ -34,16 +37,58 @@ require 'open-uri'
 				book[:category] = folder
 				book[:description] = 'book description'
 				book[:format] = 'pdf'
- 				book[:url] =  "#{Rails.root.join('app', 'assets', 'books', 'folder', '/', "#{file}")}"
+ 				# book[:url] =  "#{Rails.root.join('app', 'assets', 'books', 'folder', '/', "#{file}")}"
+ 				book[:url] =  "https://github.com/smithWEBtek/books/blob/master/" + "#{folder}" + "/" + "#{file}"
 				books.push(book)
 			end
 		end
 		Ref.import_books(books)
 	end
 	 
-
-
 	
+	def self.gitbooks
+		folders = []
+		site = HTTParty.get('https://github.com/smithWEBtek/books')
+		parse = Nokogiri::HTML(site)
+		parse.css('span.css-truncate').each do |item|
+			if item.children[0].content.match(/^\S+/) != nil
+				matchedItem = item.children[0].content.match(/^\S+/).string
+				folders.push(matchedItem)
+				
+			# binding.pry
+			# puts item.children.text
+			# puts item.children.css('a').text
+
+    # refs = parse.css('a').each do |item|
+		# 	ref = {
+		# 		title: item.text, 
+		# 		category: item.children.text,
+		# 		format: 'bookmark',
+    #     url: item.values.first,
+		# 		description: 'bookmark'
+		# 	}
+		# 	bookmarks.push(ref)
+			end
+		end
+
+		puts folders
+
+		binding.pry
+    # Ref.import_bookmarks(bookmarks)
+	end
+	
+	def self.testpdf
+		pdf = 'https://github.com/smithWEBtek/books/blob/master/algorithm/Algorithms.pdf'
+		File.open(pdf, "rb") do |io|
+			reader = PDF::Reader.new(io)	
+			puts reader.pdf_version
+			puts reader.info
+			puts reader.metadata
+			puts reader.page_count
+		end
+	end
+
+
   # def self.free_books
   #   @bookmarks = []
   #   page = HTTParty.get("https://github.com/vhf/free-programming-books/blob/master/free-programming-books.md#ruby")
--- lib/notes/asdf.md
+++ lib/notes/asdf.md
@@ -1,6 +1,39 @@
 =======================================================
 #MY TODO LIST for today
 
+	1. Talk less, listen more, be more "impressed" than "impressive"
+	2. Uplift and encourage through words and actions
+	3. Assume the best of people first, (review 1 and 2)
+	4. Before responding, Read, Listen, Pause (review 1-3)
+	
+
+invoice st mary for 2/27/2018
+
+add libary to its own git repo
+	v1
+		set up scrape of that library into swt-v3 refs
+		use library locally
+		scrape locally for folder names and filenames
+		parse together urls, (using folder names and filenames) to read books online from github
+
+	v2: 	use Cloudinary API for library upload and retrieval
+	v3:		use AWS for same, for practice
+
+get it working from Heroku, so we don't have to deal w/separate Heroku branch (sans books)
+
+learn node, mean, mongodb, express, do a project
+get auth working on swt-sp4
+get routing working on Heroku
+start using Lunar pages for something
+
+set up templates for web customers
+	db, frameworks, host, options for scaling
+	package up services
+	target markets
+	low entry for prototypes
+		get to know how we work // and we get to know how you articulate your needs 
+
+
 #House keeping
 	Invoice Heritage Baptist
 	Invoice: check on St Mary's
